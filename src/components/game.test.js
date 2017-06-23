117691d6fb507b5bb645b131e1f535241b45017d
williamtwobit <william@twobit.cc>
2017-06-23 14:42:47 -0400

    testing on parent component complete

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -23,8 +23,50 @@ describe('<Game />', ()=> {
     const wrapper = shallow(<Game />);
     const instance = wrapper.instance();
     instance.guess('');
-    //console.log(instance.feedback);
     expect(instance.state.feedback).toEqual('Please enter a valid number')
-  })
+  });
+
+  it('Should handle valid entries correctly', () => {
+    const wrapper = shallow(<Game />);
+    const instance = wrapper.instance();
+    const correct = instance.state.correctAnswer;
+    instance.guess(correct + 50);
+    expect(instance.state.feedback).toEqual('You\'re Ice Cold...');
+    instance.guess(correct + 30);
+    expect(instance.state.feedback).toEqual('You\'re Cold...');
+    instance.guess(correct + 10);
+    expect(instance.state.feedback).toEqual('You\'re Warm');
+    instance.guess(correct + 1);
+    expect(instance.state.feedback).toEqual('You\'re Hot!');
+    instance.guess(correct - 50);
+    expect(instance.state.feedback).toEqual('You\'re Ice Cold...');
+    instance.guess(correct - 30);
+    expect(instance.state.feedback).toEqual('You\'re Cold...');
+    instance.guess(correct - 10);
+    expect(instance.state.feedback).toEqual('You\'re Warm');
+    instance.guess(correct - 1);
+    expect(instance.state.feedback).toEqual('You\'re Hot!');
+    instance.guess(correct);
+    expect(instance.state.feedback).toEqual('You got it!');
+  });
+
+  it('Should update guesses array', () => {
+    const wrapper = shallow(<Game />);
+    const instance = wrapper.instance();
+
+    const testValues = [];
+
+    for(let i = 0; i < 10; i++){
+      let value = Math.floor(Math.random() * 100);
+      testValues.push(value);
+    }
+
+    testValues.forEach(value => {
+      instance.guess(value);
+    });
+
+    expect(testValues).toEqual(instance.state.guesses);
+
+  });
 
 });
